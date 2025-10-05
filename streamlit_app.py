6f15ff7f149b42f759fb1f2effefc48ad8c1ebba
Sowmya Nagarajan <155607327+Sowmyanagarajan2@users.noreply.github.com>
2025-10-05 10:42:55 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,3 +7,13 @@ from sklearn.ensemble import RandomForestClassifier
 iris = load_iris()
 model = RandomForesTClassifier().fit(iris.data,iris.target)
 st.write("Iris Predictor")
+
+a = st.number_input("Sepal Length")
+b = st.number_input("Sepal width")
+c = st.number_input("petal Length")
+d = st.number_input("Petal Widhth")
+
+if st.button("Predict"):
+   pred = model.predict([[a,b,c,d]])
+   st.write("Species:",iris.target_names[pred][0])
+  
